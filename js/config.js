905859a436ad7a00cdd74368465fc578eb5558de
anderbergr-create <anderberg.r@gmail.com>
2026-09-05 17:39:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,92 @@
 // ======================================
 // Nyhetsradar Configuration
+// Version 18.0
 // ======================================
 
 const VERSION = "18.0";
+
+
+// ======================================
+// Default Stocks
+// ======================================
+
+const defaultStocks = [
+
+  {
+    name:"Energy Fuels",
+    search:"Energy Fuels uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"Denison",
+    search:"Denison Mines uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"NexGen",
+    search:"NexGen Energy uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"Paladin",
+    search:"Paladin Energy uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"Global Atomic",
+    search:"Global Atomic Corp uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"IsoEnergy",
+    search:"IsoEnergy uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"Premier American Uranium",
+    search:"Premier American Uranium uranium",
+    type:"uran",
+    collapsed:false
+  },
+
+  {
+    name:"AbraSilver",
+    search:"AbraSilver silver",
+    type:"silver",
+    collapsed:false
+  },
+
+  {
+    name:"First Majestic",
+    search:"First Majestic Silver",
+    type:"silver",
+    collapsed:false
+  },
+
+  {
+    name:"Endeavour Silver",
+    search:"Endeavour Silver silver",
+    type:"silver",
+    collapsed:false
+  },
+
+  {
+    name:"Petrobras",
+    search:"Petrobras oil",
+    type:"energy",
+    collapsed:false
+  }
+
+];
